1d0db0c08e78e3e968150eaed3d0cea1f2147902
luanluciano93 <luanluciano@live.com>
2015-03-12 20:22:50 -0300

    Create classCarro.java

---
+++ classCarro.java
@@ -0,0 +1,112 @@
+/* Carro -> Criar uma classe encapsulada em Java que represente um Carro.
+• Nosso carro deve possuir marca, modelo, motorização, aceleração (de quantos 
+em quantos km/h o carro acelera), frenagem (de quantos em quantos km/h o 
+carro freia), velocidade atual e velocidade máxima.
+• Implementar os métodos construtores que permitam instanciar um Carro com e sem valor.
+• Implementar os métodos de acesso aos atributos;
+• O carro ainda deve possuir métodos para acelerar e frear. Estes métodos sempre 
+devem respeitar os limites de aceleração e velocidade do carro.
+*/
+
+public class Carro {
+    private String marca;
+    private String modelo;
+    private double motor;
+    private int aceleracao;
+    private int frenagem;
+    private int velocidadeAtual;
+    private int velocidadeMaxima;
+
+    public Carro(){
+        
+    }
+    
+    public Carro(String marca, String modelo, double motor, int aceleracao, int frenagem, int velocidadeMaxima) {
+        this.setMarca(marca);
+        this.setModelo(modelo);
+        this.setMotor(motor);
+        this.setAceleracao(aceleracao);
+        this.setFrenagem(frenagem);
+        this.setVelocidadeMaxima(velocidadeMaxima);
+    }    
+    
+    public void acelerar() {        
+        this.setVelocidadeAtual(this.getVelocidadeAtual() + this.getAceleracao());
+    }
+    
+    public void frear() {
+        this.setVelocidadeAtual(this.getVelocidadeAtual() - this.getFrenagem());
+    }
+    
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public double getMotor() {
+        return motor;
+    }
+
+    public void setMotor(double motor) {
+        this.motor = motor;
+    }
+
+    public int getAceleracao() {
+        return aceleracao;
+    }
+
+    public void setAceleracao(int aceleracao) {
+        this.aceleracao = aceleracao;
+    }
+
+    public int getFrenagem() {
+        return frenagem;
+    }
+
+    public void setFrenagem(int frenagem) {
+        this.frenagem = frenagem;
+    }
+
+    public int getVelocidadeAtual() {        
+        return velocidadeAtual;
+    }
+
+    public void setVelocidadeAtual(int velocidadeAtual) { 
+        // velocidade Maxima = 200
+        // aceleracao = 20
+        // velocidade atual anterior 190 (190 + 20 = 210)
+        if(velocidadeAtual >= this.getVelocidadeMaxima()) {
+            velocidadeAtual = this.getVelocidadeMaxima();
+            System.err.println("O carro está na velocidade máxima de " + this.getVelocidadeMaxima() + "km/h!");            
+        }
+        else if(velocidadeAtual <= 0) {
+            velocidadeAtual = 0;
+            System.err.println("O carro está parado!");
+        }
+        else {
+            System.out.println("Velocidade atual de " + velocidadeAtual + "km/h...");
+        }
+        this.velocidadeAtual = velocidadeAtual;
+    }
+
+    public int getVelocidadeMaxima() {
+        return velocidadeMaxima;
+    }
+
+    public void setVelocidadeMaxima(int velocidadeMaxima) {
+        this.velocidadeMaxima = velocidadeMaxima;
+    }
+    
+    
+}
